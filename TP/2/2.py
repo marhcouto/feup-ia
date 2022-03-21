db68df19a0f526944b2f881e63b6965be1afe014
marhcouto <marhcouto1101@gmail.com>
2022-03-21 23:38:49 +0000

    Finished 2

--- TP/2/2.py
+++ TP/2/2.py
@@ -1,5 +1,6 @@
 from queue import Queue
 from puzzle_state import State
+from time import time
 
 
 def final_state_matrix(size):
@@ -25,9 +26,9 @@ def find_zero(puzzle, size):
 def print_path(final_state):
     cur_state = final_state
 
-    print("\nPath:")
+    print("Path:")
     while cur_state is not None:
-        print(cur_state)
+        print(cur_state, cur_state.distance(), cur_state.depth)
         cur_state = cur_state.parent   
 
 
@@ -39,19 +40,22 @@ def bfs(puzzle):
     size = len(puzzle)
     zero = find_zero(puzzle, size)
 
-    starting_state = State(puzzle, zero, None)
+    starting_state = State(puzzle, zero, None, 0)
     final_matrix = final_state_matrix(size)
 
     q.put(starting_state)
     s.add(starting_state)
 
+    nodes_explored = 0
     while True:
+        nodes_explored += 1
 
         if q.empty():
             print("Empty queue")
             return 
 
         cur_state = q.get()
+        # print('Current state:', cur_state)
 
         if cur_state.matrix == final_matrix:
             print("Success")
@@ -59,13 +63,63 @@ def bfs(puzzle):
 
         funcs = ['zero_up', 'zero_down', 'zero_right', 'zero_left']
         for func in funcs:
-            temp = getattr(cur_state, func)()
-            if temp is not None and temp not in s:
-                q.put(temp)
-                s.add(temp)
+            new_state = getattr(cur_state, func)()
+            if new_state is not None and new_state not in s:
+                q.put(new_state)
+                s.add(new_state)
     
     print_path(cur_state)
+    print("Nodes explored:", nodes_explored)
 
+
+
+def aStar(puzzle):
+
+    q = []
+    s = set()
+    size = len(puzzle)
+    zero = find_zero(puzzle, size)
+
+    starting_state = State(puzzle, zero, None, 0)
+    final_matrix = final_state_matrix(size)
+
+    q.append(starting_state)
+    s.add(starting_state)
+
+    nodes_explored = 0
+    while True:
+        nodes_explored += 1
+
+        if len(q) == 0:
+            print("Empty queue")
+            return 
+
+        # for item in q:
+        #     print(item, item.distance() + item.depth, end=", ")
+        cur_state = q.pop(0)
+        # print('Current state:', cur_state)
+
+        if cur_state.matrix == final_matrix:
+            print("Success")
+            break
+
+        funcs = ['zero_up', 'zero_down', 'zero_right', 'zero_left']
+        for func in funcs:
+            new_state = getattr(cur_state, func)()
+            if new_state is not None and new_state not in s:
+                i = 0
+                for state in q:
+                    if new_state.distance() + new_state.depth > state.distance() + state.depth:
+                        i += 1
+                    else:
+                        break
+                q.insert(i, new_state)
+                s.add(new_state)
+
+    print_path(cur_state)
+    print("Nodes explored:", nodes_explored)
+
+    
 
 
 if __name__ == '__main__':
@@ -75,21 +129,52 @@ if __name__ == '__main__':
     puzzle_a = [[1,2,3],
                 [5,0,6],
                 [4,7,8]]
-    print("Puzzle A:\n", puzzle_a, '\n')
-
+    print("Puzzle A:\n", puzzle_a)
+    bfs_starttime = time()
     bfs(puzzle_a)
-
+    bfs_stoptime = time()
+    print("BFS took {} seconds".format(bfs_stoptime - bfs_starttime))
+    aStar_starttime = time()
+    aStar(puzzle_a)
+    aStar_stoptime = time()
+    print("A* took {} seconds\n".format(aStar_stoptime - aStar_starttime))
 
     puzzle_b = [[1,3,6],
                 [5,2,0],
                 [4,7,8]]
-    print(puzzle_b)
+    print("Puzzle B:\n", puzzle_b)
+    bfs_starttime = time()
+    bfs(puzzle_b)
+    bfs_stoptime = time()
+    print("BFS took {} seconds".format(bfs_stoptime - bfs_starttime))
+    aStar_starttime = time()
+    aStar(puzzle_b)
+    aStar_stoptime = time()
+    print("A* took {} seconds\n".format(aStar_stoptime - aStar_starttime))
+
     puzzle_c = [[1,6,2],
                 [5,7,3],
                 [0,4,8]]
-    print(puzzle_c)            
+    print("Puzzle C:\n", puzzle_c)
+    bfs_starttime = time()
+    bfs(puzzle_c)
+    bfs_stoptime = time()
+    print("BFS took {} seconds".format(bfs_stoptime - bfs_starttime))
+    aStar_starttime = time()
+    aStar(puzzle_c)
+    aStar_stoptime = time()
+    print("A* took {} seconds\n".format(aStar_stoptime - aStar_starttime))     
+
     puzzle_d = [[5,1,3,4],
                 [2,0,7,8],
                 [10,6,11,12],
                 [9,13,14,15]]
-    print(puzzle_d)
+    print("Puzzle D:\n", puzzle_d)
+    bfs_starttime = time()
+    bfs(puzzle_d)
+    bfs_stoptime = time()
+    print("BFS took {} seconds".format(bfs_stoptime - bfs_starttime))
+    aStar_starttime = time()
+    aStar(puzzle_d)
+    aStar_stoptime = time()
+    print("A* took {} seconds\n".format(aStar_stoptime - aStar_starttime))
--- TP/2/puzzle_state.py
+++ TP/2/puzzle_state.py
@@ -2,11 +2,12 @@ from copy import copy, deepcopy
 
 
 class State:
-    def __init__(self, matrix, zero, parent):
+    def __init__(self, matrix, zero, parent, depth):
         self.zero = zero
         self.matrix = matrix
         self.size = len(matrix)
         self.parent = parent
+        self.depth = depth
 
     def zero_up(self):
         if self.zero[0] == 0:
@@ -15,7 +16,7 @@ class State:
         new_matrix = deepcopy(self.matrix)
         new_matrix[self.zero[0]][self.zero[1]] = new_matrix[self.zero[0] - 1][self.zero[1]]
         new_matrix[self.zero[0] - 1][self.zero[1]] = 0
-        return State(new_matrix, new_zero, self)
+        return State(new_matrix, new_zero, self, self.depth + 1)
 
     def zero_down(self):
         if self.zero[0] == self.size - 1:
@@ -24,7 +25,7 @@ class State:
         new_matrix = deepcopy(self.matrix)
         new_matrix[self.zero[0]][self.zero[1]] = new_matrix[self.zero[0] + 1][self.zero[1]]
         new_matrix[self.zero[0] + 1][self.zero[1]] = 0
-        return State(new_matrix, new_zero, self)
+        return State(new_matrix, new_zero, self, self.depth + 1)
 
     def zero_right(self):
         if self.zero[1] == self.size - 1:
@@ -33,7 +34,7 @@ class State:
         new_matrix = deepcopy(self.matrix)
         new_matrix[self.zero[0]][self.zero[1]] = new_matrix[self.zero[0]][self.zero[1] + 1]
         new_matrix[self.zero[0]][self.zero[1] + 1] = 0
-        return State(new_matrix, new_zero, self)
+        return State(new_matrix, new_zero, self, self.depth + 1)
 
     def zero_left(self):
         if self.zero[1] == 0:
@@ -42,12 +43,16 @@ class State:
         new_matrix = deepcopy(self.matrix)
         new_matrix[self.zero[0]][self.zero[1]] = new_matrix[self.zero[0]][self.zero[1] - 1]
         new_matrix[self.zero[0]][self.zero[1] - 1] = 0
-        return State(new_matrix, new_zero, self)
+        return State(new_matrix, new_zero, self, self.depth + 1)
 
-    def distance(self, other):
+    def distance(self):
+        distance = 0
 
         for i in range(0, self.size):
             for j in range(0, self.size):
+                distance += abs(self.matrix[i][j] - ((i * self.size + j + 1) % (self.size * self.size)))
+
+        return distance
 
     
     def __eq__(self, other):
